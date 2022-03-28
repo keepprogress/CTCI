bf62d8c6df362c165cc38974e9b4fbe58461fc25
keepprogress <keepprogress@gmail.com>
2022-03-28 23:43:48 +0800

    solution to question 1

---
+++ src/Ch1/Q1.java
@@ -0,0 +1,22 @@
+package Ch1;
+
+public class Q1 {
+    // assuming the String is ASCII
+    // assuming that there are 256 characters
+
+    boolean isUniqueChars(String str) {
+        if (str.length() > 128) {
+            return false;
+        }
+
+        boolean[] char_set = new boolean[128];
+        for(int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i);
+            if(char_set[val]) {
+                return false;
+            }
+            char_set[val] = true;
+        }
+        return true;
+    }
+}
