7a9571b345e1a301812d17ebc8e5ca0e70a6cc94
keepprogress <keepprogress@gmail.com>
2022-12-19 22:53:25 +0800

    Add Q8 comments

--- src/CH2/Q8_Loop_Detection.java
+++ src/CH2/Q8_Loop_Detection.java
@@ -32,4 +32,21 @@ public class Q8_Loop_Detection {
 
         return fast;
     }
+
+    // when slow runner run k steps => the fast runner run 2k steps
+    // and the fast runner is k steps into loop
+    // if the circle length less than k
+    // then the fast runner will run M * circle length + K = k steps
+    // and that means slow runner are loop length - k steps away from each other
+    // slow runner will catch 1 step closer each turn
+    // slow runner will meet fast runner after loop length - K turns
+
+    // after slow runner meet fast runner
+    // the collided spot will be K steps before the loop start
+    // (because slow runner already run away from the loop start length -K steps implies that length - K - K = 0)
+    // it means that the collided spot is k steps long to the loop start
+
+    // if we put slow runner back to head
+    // and move slow and fast at same speed
+    // then slow runner will meet fast runner at loop start after k steps
 }
